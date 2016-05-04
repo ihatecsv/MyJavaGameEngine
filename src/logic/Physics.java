bd60a944f98e4a6430fc08b03fa7101fa24f88cf
ividito <ividitodev@gmail.com>
2016-05-04 17:30:01 -0300

    Add permutability
    Allows for a physics object to persist through all coordinates (which helps, since some objects should have constant properties, i.e. bullets shouldn't fall upon entering a zone with vertical gravity)

--- src/logic/Physics.java
+++ src/logic/Physics.java
@@ -21,6 +21,16 @@ public class Physics {
         this.dragCoeff = dragCoeff
     }
     
+    public void Physics(double yPull, double xPull, double dragCoeff){
+        //Define "Gravity Laws"
+        this.yPull = yPull;
+        this.xPull = xPull;
+        //Define drag in area
+        this.dragCoeff = dragCoeff
+    }
+    
+    
+    
     //HERE'S AN IDEA. Make each object have a Physics() object associated with it, which it uses for its behavior.
     //The issue with that is that it needs exception handlers for if it leaves a physics zone. Maybe make it default to a zone with 0 physics.
     //The other issue is if advanced 2D gravity is needed, i.e. gravity towards a point, rather than on a plane. Perhaps an AdvPhysics() class?
