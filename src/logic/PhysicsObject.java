0d2732e5d265fff0763e6b7a9eb3196fa961f316
ividito <ividitodev@gmail.com>
2016-05-04 17:43:56 -0300

    Update PhysicsObject.java
    - Removed rot variable (building basics before getting into that)
    - Added changePhysics() method to change the laws of physics obeyed by an object
    - Objects can be named now
    - Added horizontal gravity handlers
    - Added drag. Note, that the dragCoeff is actually used as a terminal velocity.

--- src/logic/PhysicsObject.java
+++ src/logic/PhysicsObject.java
@@ -1,23 +1,43 @@
 package logic;
 
+// TODO rework drag, let it adjust slower
+
 public class PhysicsObject {
     public double xVelocity;
     public double yVelocity;
     public double x;
     public double y;
-    public double rot;
+    public Physics physics;
+    public String objName;
 
-    public PhysicsObject(double x, double y, double xVelocity, double yVelocity, double rot){
+    public PhysicsObject(double x, double y, double xVelocity, double yVelocity, String objName){
         this.xVelocity = xVelocity;
         this.yVelocity = yVelocity;
         this.x = x;
         this.y = y;
-        this.rot = rot;
+        this.objName = objName;
     }
 
+    public void changePhysics(Physics newPhysics){
+        this.physics = newPhysics;
+    }
+    public void changePhysics(){
+        this.physics = new Physics(0,0,0); // Null Physics
+    }
+    
     public void simulate(double time){
-        y = (yVelocity * time) + (0.5*Physics.GRAVITY*time*time);
-        yVelocity = yVelocity + Physics.GRAVITY * time;
-        System.out.println(y + "/" + yVelocity);
+        y = (yVelocity * time) + (0.5*physics.yPull*time*time);
+        yVelocity = yVelocity + physics.yPull * time;
+        if (yVelocity>dragCoeff){
+            yVelocity = yVelocity-((dragCoeff-yVelocity)*time) // yVelocity returns to terminal velocity in 1s
+        }
+        System.out.println(objName+ " : " y + " / " + yVelocity);
+        
+        x = (xVelocity * time) + (0.5*physics.xPull*time*time);
+        xVelocity = xVelocity + physics.xPull * time;
+        if (xVelocity>dragCoeff){
+            xVelocity = xVelocity-((dragCoeff-xVelocity)*time) // xVelocity returns to terminal velocity in 1s
+        }
+        System.out.println(objName+ " : " x + " / " + xVelocity);
     }
 }
